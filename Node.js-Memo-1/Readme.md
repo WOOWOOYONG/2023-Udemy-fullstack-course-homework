4472c16503142f481bc7ee502a69431e23ff455d
woowooyong <woowooyong@gmail.com>
2022-12-28 20:27:15 +0800

    Node.js-Memo

---
+++ Node.js-Memo-1/Readme.md
@@ -0,0 +1,1 @@
+# Express + ejs 簡單練習
---
+++ Node.js-Memo-1/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //模擬從database取得資料
+  const party = [
+    { name: "凌晨晚餐", level: 66, classes: "劍士" },
+    { name: "鈣質不足", level: 77, classes: "法師" },
+    { name: "森林男孩", level: 88, classes: "盜賊" },
+  ];
+  res.render("index", { party });
+});
+
+app.get("/example", (req, res) => {
+  //從req.query解構出name及level
+  let { name, level, classes } = req.query;
+  //傳送至回應頁面
+  res.render("response", { name, level, classes });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
